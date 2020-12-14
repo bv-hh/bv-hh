aa06dbe11b81d74f06f8f7bc34b4c7335c0572b2
Timo B. Kranz <me@tbk.name>
2020-12-14 19:35:17 +0100

    Fix auth

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -1,7 +1,7 @@
 class DistrictsController < ApplicationController
 
   def show
-    redirect_to district_path(district: District.first) and return if @district.nil?
+    redirect_to root_with_district_path(district: District.first) and return if @district.nil?
 
     @documents = @district.documents.latest_first.limit(10)
     @meetings = @district.meetings.latest_first.limit(10)
