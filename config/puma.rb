1d5d98da58667ec3c80a48f960b522d01abe6193
Timo B. Kranz <me@tbk.name>
2020-12-17 15:50:58 +0100

    Fix puma

--- config/puma.rb
+++ config/puma.rb
@@ -41,7 +41,7 @@ before_fork do
   ActiveRecord::Base.connection_pool.disconnect! if defined?(ActiveRecord)
 end
 
-on_worker_book do
+on_worker_boot do
   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
 end
 
