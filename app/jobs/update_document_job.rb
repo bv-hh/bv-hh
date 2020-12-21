b2eddcde18fa6a71e0bea7a2cba4462846f48128
Timo B. Kranz <me@tbk.name>
2020-12-21 21:52:57 +0100

    Use custom queues

--- app/jobs/update_document_job.rb
+++ app/jobs/update_document_job.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class UpdateDocumentJob < ApplicationJob
+  queue_as :documents
+
   def perform(document)
     document.retrieve_from_allris
     document.save!
--- app/jobs/update_meeting_job.rb
+++ app/jobs/update_meeting_job.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class UpdateMeetingJob < ApplicationJob
+  queue_as :meetings
+
   def perform(meeting)
     meeting.agenda_items.delete_all
     meeting.retrieve_from_allris
