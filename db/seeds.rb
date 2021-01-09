cd5be0c2fa2df95e27560a26ebb5543b1da651d2
Timo B. Kranz <me@tbk.name>
2021-01-09 22:59:04 +0100

    Seeding all districts

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,4 +11,20 @@ District.create! name: "Hamburg-Nord", allris_base_url: "https://sitzungsdienst-
   oldest_allris_document_id: 1007791, oldest_allris_meeting_date: "2019-01-01"
 
 District.create! name: "Wandsbek", allris_base_url: "https://sitzungsdienst-wandsbek.hamburg.de",
-  oldest_allris_document_id: 1013300, oldest_allris_meeting_date: "2019-01-01"
+  oldest_allris_document_id: 1010480, oldest_allris_meeting_date: "2019-01-01"
+
+District.create! name: "Altona", allris_base_url: "https://sitzungsdienst-altona.hamburg.de",
+  oldest_allris_document_id: 1008906, oldest_allris_meeting_date: "2019-01-01"
+
+District.create! name: "Hamburg-Mitte", allris_base_url: "https://sitzungsdienst-hamburg-mitte.hamburg.de",
+  oldest_allris_document_id: 1011795, oldest_allris_meeting_date: "2019-01-01"
+
+District.create! name: "Eimsbüttel", allris_base_url: "https://sitzungsdienst-eimsbüttel.hamburg.de",
+  oldest_allris_document_id: 1004634, oldest_allris_meeting_date: "2019-01-01"
+
+District.create! name: "Harburg", allris_base_url: "https://sitzungsdienst-harburg.hamburg.de",
+  oldest_allris_document_id: 1006690, oldest_allris_meeting_date: "2019-01-01"
+
+District.create! name: "Bergedorf", allris_base_url: "https://sitzungsdienst-bergedorf.hamburg.de",
+  oldest_allris_document_id: 1004374, oldest_allris_meeting_date: "2019-01-01"
+
