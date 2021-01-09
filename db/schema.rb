4a3cdc8a88d6d116dd0c8f739660e7aa3222014c
Timo B. Kranz <me@tbk.name>
2021-01-09 23:51:07 +0100

    Add some more stats

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_30_213900) do
+ActiveRecord::Schema.define(version: 2021_01_09_223355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 2020_12_30_213900) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.date "oldest_allris_meeting_date"
+    t.string "first_legislation_number"
   end
 
   create_table "documents", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,23 +8,23 @@
 #
 
 District.create! name: "Hamburg-Nord", allris_base_url: "https://sitzungsdienst-hamburg-nord.hamburg.de",
-  oldest_allris_document_id: 1007791, oldest_allris_meeting_date: "2019-01-01"
+  oldest_allris_document_id: 1007791, oldest_allris_meeting_date: "2019-01-01", first_legislation_number: '21-0001'
 
 District.create! name: "Wandsbek", allris_base_url: "https://sitzungsdienst-wandsbek.hamburg.de",
-  oldest_allris_document_id: 1010480, oldest_allris_meeting_date: "2019-01-01"
+  oldest_allris_document_id: 1010480, oldest_allris_meeting_date: "2019-01-01", first_legislation_number: '21-0001'
 
 District.create! name: "Altona", allris_base_url: "https://sitzungsdienst-altona.hamburg.de",
-  oldest_allris_document_id: 1008906, oldest_allris_meeting_date: "2019-01-01"
+  oldest_allris_document_id: 1008906, oldest_allris_meeting_date: "2019-01-01", first_legislation_number: '21-0001'
 
 District.create! name: "Hamburg-Mitte", allris_base_url: "https://sitzungsdienst-hamburg-mitte.hamburg.de",
-  oldest_allris_document_id: 1011795, oldest_allris_meeting_date: "2019-01-01"
+  oldest_allris_document_id: 1011795, oldest_allris_meeting_date: "2019-01-01", first_legislation_number: '22-0001'
 
 District.create! name: "Eimsbüttel", allris_base_url: "https://sitzungsdienst-eimsbüttel.hamburg.de",
-  oldest_allris_document_id: 1004634, oldest_allris_meeting_date: "2019-01-01"
+  oldest_allris_document_id: 1004634, oldest_allris_meeting_date: "2019-01-01", first_legislation_number: '21-0001'
 
 District.create! name: "Harburg", allris_base_url: "https://sitzungsdienst-harburg.hamburg.de",
-  oldest_allris_document_id: 1006690, oldest_allris_meeting_date: "2019-01-01"
+  oldest_allris_document_id: 1006690, oldest_allris_meeting_date: "2019-01-01", first_legislation_number: '21-0001'
 
 District.create! name: "Bergedorf", allris_base_url: "https://sitzungsdienst-bergedorf.hamburg.de",
-  oldest_allris_document_id: 1004374, oldest_allris_meeting_date: "2019-01-01"
+  oldest_allris_document_id: 1004374, oldest_allris_meeting_date: "2019-01-01", first_legislation_number: '21-0001'
 
