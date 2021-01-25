6468895cdf874ac14a8de0e17293c4c3809e6484
Timo B. Kranz <me@tbk.name>
2021-01-25 12:20:53 +0100

    Fix blazer auth

--- config/initializers/blazer.rb
+++ config/initializers/blazer.rb
@@ -2,6 +2,6 @@
 
 blazer_auth = Rails.application.credentials.dig(Rails.env.to_sym, :admin_auth)
 if blazer_auth
-  ENV['BLAZER_USERNAME'] = auth[:username]
-  ENV['BLAZER_PASSWORD'] = auth[:password]
+  ENV["BLAZER_USERNAME"] = blazer_auth[:username]
+  ENV["BLAZER_PASSWORD"] = blazer_auth[:password]
 end
