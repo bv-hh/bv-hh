68707ab0031d5541dad949a65e733eec68c5d23b
Timo B. Kranz <me@tbk.name>
2024-04-11 16:47:49 +0200

    Use credentials for openai api key

--- config/initializers/qdrant_db.rb
+++ config/initializers/qdrant_db.rb
@@ -2,7 +2,7 @@ class QdrantDb
   attr_reader :connection
 
   def initialize
-    llm = Langchain::LLM::OpenAI.new(api_key: ENV["OPENAI_API_KEY"])
+    llm = Langchain::LLM::OpenAI.new(api_key: Rails.application.credentials.openai_api_key)
 
     @connection = Langchain::Vectorsearch::Qdrant.new(
       url: ENV['QDRANT_URL'],
