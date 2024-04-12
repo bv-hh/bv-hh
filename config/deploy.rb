fcc635284adbfdab6e7d7f07c50adb7ca1d69d99
Christoph Eicke <christoph@geisterstunde.org>
2024-04-12 11:18:53 +0200

    Update Ruby deployment version

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :branch, ENV.fetch('branch', nil) || current_branch || 'master'
 set :deploy_to, '/home/deploy/app'
 
 set :rbenv_type, :user
-set :rbenv_ruby, '3.2.2'
+set :rbenv_ruby, '3.2.3'
 
 set :migration_role, :web
 
