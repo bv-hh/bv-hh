b894a54f27303c91b03c7073c6c6ad2d49f496de
Christoph Eicke <christoph@geisterstunde.org>
2024-04-12 15:12:01 +0200

    Provide some prompt engineering around the question.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,7 +9,7 @@ class QuestionsController < ApplicationController
   def create
     @question = question
     qdrant = QdrantDb.new
-    res = qdrant.connection.ask(question: @question)
+    res = qdrant.connection.ask(question: wrap_question_in_default_prompt(question))
     
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
     @answer = markdown.render(res.raw_response['choices'].first['message']['content'])
@@ -18,6 +18,14 @@ class QuestionsController < ApplicationController
   private
   def question
     params[:question][:question]
+  end
+
+  def wrap_question_in_default_prompt(question)
+    "You are a helpful press official, politely and swiftly answering questions from interested citizens.\
+    You can only make conversations based on the provided context. \
+    If a response cannot be formed strictly using the context, politely say you don’t have knowledge about that topic. \
+    Answer in the language that the question is in.\
+    Here comes the question: #{question}"
   end
 
 end
