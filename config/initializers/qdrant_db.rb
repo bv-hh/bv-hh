1355d42be78c0ae2cd15894805b67bb54b741e53
Christoph Eicke <christoph@geisterstunde.org>
2024-04-12 15:02:48 +0200

    Switch to GPT-4 model

--- config/initializers/qdrant_db.rb
+++ config/initializers/qdrant_db.rb
@@ -2,7 +2,13 @@ class QdrantDb
   attr_reader :connection
 
   def initialize
-    llm = Langchain::LLM::OpenAI.new(api_key: Rails.application.credentials.openai_api_key)
+    llm = Langchain::LLM::OpenAI.new(
+      api_key: Rails.application.credentials.openai_api_key,
+      llm_options: {
+        chat_completion_model_name: 'gpt-4-turbo',
+        embeddings_model_name: 'text-embedding-3-small'
+      }
+    )
 
     @connection = Langchain::Vectorsearch::Qdrant.new(
       url: ENV.fetch('QDRANT_URL', 'http://qdrant:6333'),
@@ -10,6 +16,7 @@ class QdrantDb
       index_name: 'bezirkr',
       llm: llm
     )
+    @connection.create_default_schema
   end
 
 end
