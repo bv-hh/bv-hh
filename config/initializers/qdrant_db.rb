3d1b7c061423d20ba892e2625186be01b07b2c03
Christoph Eicke <christoph@geisterstunde.org>
2024-04-12 14:56:53 +0200

    Make the environment fetching more robust

--- config/initializers/qdrant_db.rb
+++ config/initializers/qdrant_db.rb
@@ -5,8 +5,8 @@ class QdrantDb
     llm = Langchain::LLM::OpenAI.new(api_key: Rails.application.credentials.openai_api_key)
 
     @connection = Langchain::Vectorsearch::Qdrant.new(
-      url: 'http://qdrant:6333',
-      api_key: ENV['QDRANT_API_KEY'],
+      url: ENV.fetch('QDRANT_URL', 'http://qdrant:6333'),
+      api_key: ENV.fetch('QDRANT_API_KEY', ''),
       index_name: 'bezirkr',
       llm: llm
     )
