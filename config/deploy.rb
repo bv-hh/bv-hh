cc1da86f28a1cfb1e5e251a8afb079c5bd626da2
Timo B. Kranz <timo@kranz.land>
2024-10-08 22:58:51 +0200

    Fix typo

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # config valid for current version and patch releases of Capistrano
-lock '~> 3.19.2'
+lock '~> 3.19.1'
 
 set :application, 'bezirkr'
 set :repo_url, 'git@github.com:bv-hh/bv-hh.git'
