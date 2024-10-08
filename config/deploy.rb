24046655a18ee2e19ee9c517dab755376aa43871
Timo B. Kranz <timo@kranz.land>
2024-10-08 22:58:30 +0200

    Update Capfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # config valid for current version and patch releases of Capistrano
-lock '~> 3.18.0'
+lock '~> 3.19.2'
 
 set :application, 'bezirkr'
 set :repo_url, 'git@github.com:bv-hh/bv-hh.git'
