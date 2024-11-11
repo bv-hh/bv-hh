cc39576df9b59f4166ccf1da7c2f79d1ad29a01f
Timo B. Kranz <timo@kranz.land>
2024-11-11 21:30:25 +0100

    Add good_jobs

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ Bundler.require(*Rails.groups)
 module Bezirkr
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.2
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
@@ -25,5 +25,7 @@ module Bezirkr
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.active_job.queue_adapter = :good_job
   end
 end
