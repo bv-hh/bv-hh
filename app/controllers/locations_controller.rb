64d8c8fd85cda11b215d0e49df9d69f7aead7917
Timo B. Kranz <timo@kranz.land>
2024-12-18 23:47:28 +0100

    Avoid duplicates on location page

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -7,7 +7,7 @@ class LocationsController < ApplicationController
     full_location_path = location_path(@location, district: @location.district)
     redirect_to(full_location_path, status: :moved_permanently) and return unless request.path == full_location_path
 
-    @documents = @location.documents.complete.include_meetings.latest_first.page(params[:page])
+    @documents = @location.documents.complete.include_meetings.latest_first.distinct.page(params[:page])
 
     @title = "#{@location.name} - #{@location.formatted_address}"
   end
