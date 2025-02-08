b01095dd8d518879cf8c8134055577051aefe0ec
Timo B. Kranz <timo@kranz.land>
2025-02-08 22:15:52 +0100

    linting

--- app/models/district.rb
+++ app/models/district.rb
@@ -72,7 +72,7 @@ class District < ApplicationRecord
   end
 
   def check_for_meeting_updates
-    oldest_meeting_date = meetings.complete.present? ? Date.today : oldest_allris_meeting_date
+    oldest_meeting_date = meetings.complete.present? ? Time.zone.today : oldest_allris_meeting_date
     oldest_meeting_date = (oldest_meeting_date - 1.month).beginning_of_month
 
     current_date = 9.months.from_now.beginning_of_month
