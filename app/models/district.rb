1fab7eb8d92a5c082da9d7d07ed8cf6d39b48a1f
Timo B. Kranz <timo@kranz.land>
2025-02-12 23:30:00 +0100

    Meeting attachments (closes #604)

--- app/models/district.rb
+++ app/models/district.rb
@@ -72,8 +72,9 @@ class District < ApplicationRecord
   end
 
   def check_for_meeting_updates
-    oldest_meeting_date = meetings.complete.present? ? Time.zone.today : oldest_allris_meeting_date
-    oldest_meeting_date = (oldest_meeting_date - 1.month).beginning_of_month
+    oldest_meeting_date = meetings.complete.latest_first.first&.date
+    oldest_meeting_date ||= oldest_allris_meeting_date
+    oldest_meeting_date = (oldest_meeting_date - 2.months).beginning_of_month
 
     current_date = 9.months.from_now.beginning_of_month
 
